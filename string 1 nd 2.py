39fbcb38ab8202032d9d352305f66477def53a63
kanishka <skanishka486@gmail.com>
2024-09-05 09:07:19 +0530

    Add files via upload

---
+++ string 1 nd 2.py
@@ -0,0 +1,8 @@
+def check_if_can_break(s1, s2):
+    s1_sorted = sorted(s1)
+    s2_sorted = sorted(s2)
+    
+    if all(s1_char >= s2_char for s1_char, s2_char in zip(s1_sorted, s2_sorted)) or all(s1_char <= s2_char for s1_char, s2_char in zip(s1_sorted, s2_sorted)):
+        return True
+    else:
+        return False
