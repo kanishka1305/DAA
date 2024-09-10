f616421e20911b7467cd9aabf58fe9ba513ee81e
kanishka <skanishka486@gmail.com>
2024-09-10 10:05:56 +0545

    Add files via upload

---
+++ largest num.py
@@ -0,0 +1,5 @@
+def find_largest_element(arr):
+    return max(arr)
+array = [10, 30, 20, 50, 40]
+largest_element = find_largest_element(array)
+print("The largest element in the array is:", largest_element)
