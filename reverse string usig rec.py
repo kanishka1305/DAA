7bc68eb83d0394e5c45c7987f59dc6fcb55a45cd
kanishka <skanishka486@gmail.com>
2024-09-10 10:04:54 +0545

    Add files via upload

---
+++ reverse string usig rec.py
@@ -0,0 +1,8 @@
+def reverse_string(s):
+    if len(s) == 0:
+        return s
+    else:
+        return reverse_string(s[1:]) + s[0]
+input_string = "Hello, World!"
+reversed_string = reverse_string(input_string)
+print(reversed_string)
