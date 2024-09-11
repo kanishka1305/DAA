66dff663f5cbe8b477889366ae12d6f26ced7017
kanishka <skanishka486@gmail.com>
2024-09-11 23:28:15 +0530

    Add files via upload

---
+++ peak element.py
@@ -0,0 +1,16 @@
+def find_peak_element(nums):
+    left, right = 0, len(nums) - 1
+    
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] < nums[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+            
+    return left
+
+# Example usage
+nums = [1, 2, 3, 1]
+peak_index = find_peak_element(nums)
+print(peak_index)
