bf2d034efe6932b6e3d931fbe331795001eeb3a1
kanishka <skanishka486@gmail.com>
2024-09-17 13:02:59 +0530

    Create pro 53.py

---
+++ pro 53.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+print(bubble_sort([-3, -1, -2, -4]))  
